7415798bc53d2767f1dd07e26a68bf8137a7a817
silviapetrino <petrino.silvia@gmail.com>
2023-12-06 15:49:37 +0100

    [add] call to api

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+import { store } from './data/store';
+import axios from 'axios';
+import projectCard from './components/projectCard.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      projectCard
+    },
+    data(){
+      return {
+        store
+      }
+    },
+    methods: {
+      getApi(){
+        axios.get(store.apiUrl)
+        .then(results => {
+            console.log(results.data);
+          }).catch(error => 
+            console.log(error));
+          }
+      },
+
+      mounted(){
+        this.getApi();
+      }
+    }
+  
+
+</script>
+
+<template>
+
+
+<div class="container">
+  <h1>Project List</h1>
+  <projectCard />
+</div>
+
+</template>
+
+<style lang="scss" scoped>
+
+body {
+  background-color: red;
+}
+
+
+</style>
---
+++ src/components/projectCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: 'projectCard'
+}
+</script>
+
+<template>
+
+  <div class="card">
+    <h2>Card</h2>
+  </div>
+  
+
+
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
